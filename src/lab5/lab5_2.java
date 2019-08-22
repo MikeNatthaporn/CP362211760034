26f6353c10d4067065ea8c2f7f60fc0f6aa14e2b
MikeNatthaporn <natthapon.26082541@gmail.com>
2019-08-22 16:34:29 +0700

    lab6

---
+++ src/lab5/lab5_2.java
@@ -0,0 +1,31 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class lab5_2 {
+    public static void main (String[] args){
+        Scanner sc =new Scanner(System.in);
+        final double PI = 3.141;
+
+        System.out.print("Enter r: ");
+        double r = sc.nextDouble();
+
+        double c = 2 * PI * r;
+        System.out.println("Circle round: "+ c);
+
+        final  double P = 2;
+        double v =(4/3) *PI *r*r*r;
+        System.out.println("ปริมาตรวงกลม: "+v);
+        final double D = 1.33;
+        double a =4.18*r;
+        System.out.println("Circle round: "+a);
+
+
+
+
+    }//main
+
+
+
+
+}//class
---
+++ src/lab5/lab5_3.java
@@ -0,0 +1,28 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class lab5_3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int day ;
+        long hr,minute,seconds;
+        System.out.println("Enter seconds:");
+        long input_seconds =sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
